7686b7c79660dcd86456057777393a2a5cb6556a
AchrefFast <justnevergiveup001@gmail.com>
2022-03-20 07:17:28 +0100

    Fixed escaped error

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Head from "next/head";
 import { useState, Fragment, useContext } from "react";
-import CommentItem from "../components/Comments/commentItem";
-import NewCommentForm from "../components/Form/NewCommentForm";
-import AppContext from "../components/store/app-context";
+import CommentItem from "../components/Comments/commentItem.js";
+import NewCommentForm from "../components/Form/NewCommentForm.js";
+import AppContext from "../components/store/app-context.js";
 import classes from "../styles/main.module.scss";
 
 export default function Home() {
